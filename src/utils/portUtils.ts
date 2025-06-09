c3e8f7e4000302f623b546cb50672e4360087f06
helenanull <helena.github@gmail.com>
2025-06-09 23:43:48 +0200

    test2

--- src/utils/portUtils.ts
+++ src/utils/portUtils.ts
@@ -1,28 +1,26 @@
 import detect from "detect-port";
 import chalk from "chalk";
 
-const backendPort = process.env.VITE_BACKEND_PORT || "4000";
-const frontendPort = process.env.VITE_FRONTEND_PORT || "5173";
+export const backendPort = process.env.VITE_BACKEND_PORT || "4000";
 
-export const getBackendPort = async () => {
-  return detect(Number(backendPort))
-    .then((_port) => {
-      if (Number(backendPort) === _port) {
-        console.log(chalk.green(`Backend server running at http://localhost:${backendPort}`));
-        return Number(backendPort);
-      }
-
-      console.log(
-        chalk.red(
-          `Failed to start the backend server on port ${backendPort}. \n Starting the backend server on port ${_port}. \n Please update VITE_BACKEND_PORT in the .env file and 'apiUrl' in cypress.json to ${_port}.`
-        )
-      );
-      return _port;
-    })
-    .catch((err) => {
-      console.log(chalk.red(err));
+export const getBackendPort = async (): Promise<number | undefined> => {
+  try {
+    const _port = await detect(Number(backendPort));
+    if (Number(backendPort) === _port) {
+      console.log(chalk.green(`Backend server running at http://localhost:${backendPort}`));
       return Number(backendPort);
-    });
-};
+    }
 
-export { frontendPort };
+    console.log(
+      chalk.red(
+        `Failed to start the backend server on port ${backendPort}.\n` +
+          `Starting the backend server on port ${_port} instead.\n` +
+          `Please update VITE_BACKEND_PORT in your .env and 'apiUrl' in cypress.json to ${_port}.`
+      )
+    );
+    return _port;
+  } catch (err) {
+    console.log(chalk.red(`Error detecting port: ${err}`));
+    return undefined;
+  }
+};
